bd22c41e51bfb9cc7ab0cf966dc63ade3a1bb907
Moustafa25MM <107191669+Moustafa25MM@users.noreply.github.com>
2023-08-22 12:51:08 +0300

    create Validations using Express-Validator

---
+++ src/middlewares/validations.ts
@@ -0,0 +1,31 @@
+import { body } from 'express-validator';
+
+const checkEmail = body('email')
+  .isString()
+  .normalizeEmail()
+  .trim()
+  .notEmpty()
+  .withMessage('Email is required')
+  .matches(/^\S+@\S+\.\S+$/)
+  .withMessage('Invalid email format');
+
+const checkName = body('name')
+  .isString()
+  .exists({ checkFalsy: false })
+  .withMessage('name is required')
+  .trim()
+  .isLength({ min: 5, max: 50 })
+  .withMessage('name: must be at least 5 chars long & maximum 50 chars');
+
+const checkPassowrd = body('password')
+  .exists({ checkFalsy: true })
+  .withMessage('Password is required')
+  .trim()
+  .isLength({ min: 5, max: 1024 })
+  .withMessage('Password: must be at least 5 chars longs');
+
+export const validations = {
+  checkEmail,
+  checkName,
+  checkPassowrd,
+};
