d7a1a2d6dc3c948491e7e821e97a9e9c4ef9baa4
Moustafa25MM <107191669+Moustafa25MM@users.noreply.github.com>
2023-08-22 03:37:11 +0300

    Creating Employee Schema

---
+++ src/models/employee.ts
@@ -0,0 +1,52 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+enum EmployeeGroup {
+  HR = 'HR',
+  NORMAL_EMPLOYEE = 'Normal Employee',
+}
+
+interface IEmployee extends Document {
+  name: string;
+  email: string;
+  group: EmployeeGroup;
+}
+
+const EmployeeSchema: Schema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minLength: 5,
+      maxLength: 50,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 5,
+      maxLength: 1024,
+    },
+    email: {
+      type: String,
+      required: true,
+      minLength: 5,
+      maxLength: 255,
+      unique: true,
+    },
+    group: {
+      type: String,
+      required: true,
+      enum: Object.values(EmployeeGroup),
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      transform(doc, ret) {
+        delete ret.password;
+      },
+    },
+  }
+);
+
+const Employee = mongoose.model('employee', EmployeeSchema);
+export default Employee;
