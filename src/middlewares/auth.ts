b22cd4c04d0fdeb18c1a841c71b18a78428e2f2a
Moustafa25MM <107191669+Moustafa25MM@users.noreply.github.com>
2023-08-22 05:54:00 +0300

    is HR Authorized implemented

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -2,6 +2,9 @@ import bcrypt from 'bcrypt';
 
 import * as jwt from 'jsonwebtoken';
 import * as dotenv from 'dotenv';
+import { NextFunction } from 'express';
+import { models } from 'mongoose';
+import { employeeControllers } from '../controllers/employee';
 
 dotenv.config();
 const JWTSecret = process.env.JWT_SECRET;
@@ -23,8 +26,41 @@ type IokenPayload = {
 
 const generateJWT = (payload: IokenPayload): String =>
   jwt.sign(payload, JWTSecret as string, { expiresIn: '7d' });
+
+const isHRAuthorized = async (req: any, res: any, next: NextFunction) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ error: 'Unauthorized: Token not provided' });
+  }
+
+  try {
+    const payload: { id: string } = jwt.verify(token, JWTSecret as string) as {
+      id: string;
+    };
+    console.log(payload.id);
+    const employeeData = await employeeControllers.getEmployeeById(payload.id);
+    if (!employeeData) {
+      return res.status(400);
+    }
+    if (employeeData.group !== 'HR') {
+      return res.status(403).json({ error: 'Forbidden: Not authorized as HR' });
+    }
+
+    req.employee = {
+      id: employeeData.id,
+      email: employeeData.email,
+      group: employeeData.group,
+    };
+
+    next();
+  } catch (error) {
+    return res.status(401).json({ error: 'Unauthorized: Invalid token' });
+  }
+};
 export const authMethods = {
   hashPassword,
   comparePasswd,
   generateJWT,
+  isHRAuthorized,
 };
--- src/routes/employee.ts
+++ src/routes/employee.ts
@@ -2,9 +2,19 @@ import { Router } from 'express';
 
 import { employeeControllers } from '../controllers/employee';
 import { employeeMiddelwares } from '../middlewares/employee';
+import { authMethods } from '../middlewares/auth';
 
 const router = Router();
 
-router.patch('/profile/:id', employeeMiddelwares.updateEmployeeFunc);
+router.post(
+  '/create',
+  authMethods.isHRAuthorized,
+  employeeMiddelwares.createEmployee
+);
+router.patch(
+  '/profile/:id',
+  authMethods.isHRAuthorized,
+  employeeMiddelwares.updateEmployeeFunc
+);
 
 export const employeeRoute: Router = router;
